a734c38905af038acb0538cc904255d8e6a36a37
RalphSaridar369 <ralphsaridar@hotmail.com>
2026-04-26 18:50:43 +0300

    Fixed Backend

--- Backend/main.py
+++ Backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from transformers import pipeline
 from contextlib import asynccontextmanager
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 class TextRequest(BaseModel):
     text: str
@@ -16,6 +17,18 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    "http://localhost:5173",  # Vite dev server
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.post("/predict")
 def predict(request: TextRequest):
     classifier = app.state.classifier
@@ -26,7 +39,14 @@ def predict(request: TextRequest):
         hypothesis_template="This phrase should belong to a {} political view."
     )
 
+    scores = []
+    if result["labels"][0] == "conservative":
+        scores = [result["scores"][1],result["scores"][0]]
+    else:   
+        scores = [result["scores"][0],result["scores"][1]]
+
+
     return {
         "label": result["labels"][0],
-        "scores": result["scores"]
+        "scores": scores
     }
